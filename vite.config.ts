3dd7e4e5e8d39b4ed91e82bc139997b4fff77851
Jessica Kuo <131988618+jesskuo4@users.noreply.github.com>
2025-09-25 18:02:38 -0700

    Refactor Vite config and set base path
    Removed unnecessary imports and updated config structure.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-})
+  base: '/whisper-web/', // 👈 repo name, must match your GitHub repo
+});
